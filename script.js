cf8f40bef738d50b94181753fa8cdbf57d19339d
qkas <kassuvaa@gmail.com>
2023-02-16 08:46:08 +0200

    aktiivinen-sivu highlightattu myös mobiililaitteilla

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const links = document.querySelectorAll(".sivut a");
+/* Tietokoneelle */
+const dLinks = document.querySelectorAll(".sivut a");
+const dCurrentURL = window.location.href;
 
-const currentURL = window.location.href;
-
-links.forEach(link => {
-if (link.href === currentURL) {
+dLinks.forEach(link => {
+if (link.href === dCurrentURL) {
     link.classList.add("active");
 }
 });
 
+/* Mobiililaitteille */
+const mLinks = document.querySelectorAll("#dropdown-sivut a");
+const mCurrentURL = window.location.href;
+
+mLinks.forEach(link => {
+if (link.href === mCurrentURL) {
+    link.classList.add("active");
+}
+});
+
+/* Dropdown menu mobiililaitteille */
 function dropdown() {
     var x = document.getElementById("dropdown-sivut");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
 }
